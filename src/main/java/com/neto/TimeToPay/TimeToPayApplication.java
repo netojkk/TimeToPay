feec1ac889d218a28e33a44262b48b008dcecee8
Neto <136371812+netojkk@users.noreply.github.com>
2025-07-06 13:43:42 -0300

    Initial commmit

---
+++ src/main/java/com/neto/TimeToPay/TimeToPayApplication.java
@@ -0,0 +1,13 @@
+package com.neto.TimeToPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeToPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeToPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neto/TimeToPay/TimeToPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neto.TimeToPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeToPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
