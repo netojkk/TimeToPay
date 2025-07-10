9a780aaeccbd26cf3a09770d30a0d48242d5a164
Neto <136371812+netojkk@users.noreply.github.com>
2025-07-09 21:40:44 -0300

    fix: fixing Signature to Subscription

--- src/main/java/com/neto/TimeToPay/domain/Subscription.java
+++ src/main/java/com/neto/TimeToPay/domain/Subscription.java
@@ -6,26 +6,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "signature")
+@Table(name = "subscription")
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Signature {
+public class Subscription {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+    private BigDecimal value;
     private LocalDate dateSigned;
-    private LocalDate dueDate;
-    private boolean active;
+    private LocalDate expirationDate;
+    private Boolean active;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private Client client;
+
 
 }
