0c64ad4bf2a4c11117055c01af6586809275847a
Neto <136371812+netojkk@users.noreply.github.com>
2025-07-09 21:38:35 -0300

    feat: implementing clients login and fetch all clients

--- src/main/java/com/neto/TimeToPay/services/ClientService.java
+++ src/main/java/com/neto/TimeToPay/services/ClientService.java
@@ -1,23 +1,26 @@
 package com.neto.TimeToPay.services;
 
 import com.neto.TimeToPay.domain.Client;
-import com.neto.TimeToPay.dto.RegisterRequestDTO;
-import com.neto.TimeToPay.dto.RegisterResponseDTO;
+import com.neto.TimeToPay.dto.*;
+import com.neto.TimeToPay.exceptions.DuplicateEmailException;
+import com.neto.TimeToPay.exceptions.UserNotFoundException;
+import com.neto.TimeToPay.exceptions.WrongPasswordException;
 import com.neto.TimeToPay.repositories.ClientRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
-@NoArgsConstructor
 public class ClientService {
 
     private ClientRepository clientRepository;
 
-    private RegisterResponseDTO createClient(RegisterRequestDTO data){
+    public RegisterResponseDTO singUpClient(RegisterRequestDTO data){
         if (clientRepository.findByEmail(data.email()).isPresent()){
-            throw new RuntimeException("Email already exists!");
+            throw new DuplicateEmailException("Email already exists!");
         }
 
         Client client = new Client(data);
@@ -25,5 +28,20 @@ public class ClientService {
         return new RegisterResponseDTO(client.getId(), client.getName(), client.getEmail());
     }
 
+    public LoginResponseDTO loginClient(LoginRequestDTO data){
+        Client client = this.clientRepository.findByEmail(data.email())
+                .orElseThrow(() -> new UserNotFoundException("User Not Found!"));
 
+        if(!data.password().equals(client.getPassword())){
+            throw new WrongPasswordException("Wrong Password!");
+        }
+
+        return new LoginResponseDTO(client.getId(), client.getName(),client.getEmail());
+    }
+
+    public List<ClientResponseDTO> getAllClients(){
+        return clientRepository.findAll().stream()
+                .map(client -> new ClientResponseDTO(client.getId(), client.getName(), client.getEmail()))
+                .toList();
+    }
 }
