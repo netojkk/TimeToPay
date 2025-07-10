6f386fd304afd5cab557c645f9b74e05300505f9
Neto <136371812+netojkk@users.noreply.github.com>
2025-07-09 21:36:34 -0300

    fix: fixing client's entity

--- src/main/java/com/neto/TimeToPay/domain/Client.java
+++ src/main/java/com/neto/TimeToPay/domain/Client.java
@@ -1,11 +1,13 @@
 package com.neto.TimeToPay.domain;
 
+import com.neto.TimeToPay.dto.ClientResponseDTO;
 import com.neto.TimeToPay.dto.RegisterRequestDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Table(name = "client")
@@ -22,9 +24,17 @@ public class Client {
     private String email;
     private String password;
 
+    //public Client (Long id, ClientResponseDTO client) {
+      //  this.id = client.id();
+     //   this.name = client.name();
+      //  this.email = client.email();
+    //}
+
     public Client (RegisterRequestDTO client){
         this.name = client.name();
         this.email = client.email();
         this.password = client.password();
     }
+
+
 }
